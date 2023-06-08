d2ab1474e9ddddb7a8b8ed11c7b2c73407cab1dc
swemu <swemu@DESKTOP-ARV1JDP>
2023-06-08 21:14:17 +0500

    meal

--- src/EcoExpresso.Domain/Entities/Meal.cs
+++ src/EcoExpresso.Domain/Entities/Meal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EcoExpresso.Domain.Commons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace EcoExpresso.Domain.Entities
 {
-    internal class Meal
+    public class Meal : Auditable
     {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+        public long CookedBy { get; set; }
+        public User User { get; set; }
+
     }
 }

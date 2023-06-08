66940e264c77585245234a75d906e9c37b54dce3
swemu <swemu@DESKTOP-ARV1JDP>
2023-06-08 21:18:10 +0500

    meal upg

--- src/EcoExpresso.Domain/Entities/Meal.cs
+++ src/EcoExpresso.Domain/Entities/Meal.cs
@@ -1,20 +1,15 @@
 ﻿using EcoExpresso.Domain.Commons;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EcoExpresso.Domain.Entities
+
+namespace EcoExpresso.Domain.Entities;
+
+public class Meal : Auditable
 {
-    public class Meal : Auditable
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public decimal Price { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
 
-        public long CookedBy { get; set; }
-        public User User { get; set; }
+    public long CookedBy { get; set; }
+    public User User { get; set; }
 
-    }
 }

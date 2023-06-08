6ab47c9dfbfce1ec1e4410c3e8ca02cd815e3573
swemu <swemu@DESKTOP-ARV1JDP>
2023-06-08 21:17:23 +0500

    user

--- src/EcoExpresso.Domain/Entities/User.cs
+++ src/EcoExpresso.Domain/Entities/User.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using EcoExpresso.Domain.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,17 @@ namespace EcoExpresso.Domain.Entities
 {
     public class User
     {
+        [MinLength(3), MaxLength(50)]
+        public string FirstName { get; set; }
+        [MinLength(3), MaxLength(50)]
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
 
+        public Role Role { get; set; } = Role.User;
+
+
+        public ICollection<Meal> CookedMeals { get; set; }
     }
 }
